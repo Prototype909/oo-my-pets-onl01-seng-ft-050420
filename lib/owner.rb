580f96890d8ef3d099ea6d51208d8f468993ca18
Steve Anderson <prototype909@msn.com>
2020-05-20 01:56:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,13 @@
 class Owner
 
+  attr_accessor :pets
+  attr_reader :name, :species
+
+  @@all = []
+
   def initialize(name)
     @name = name
+    @species = "human"
+    @@all << self
   end
 end
