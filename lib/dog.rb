a587305b3318498cc8a7e2c2fc4ae3650c7a7f2e
Steve Anderson <prototype909@msn.com>
2020-05-20 01:53:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
-class Dog
+class Cat
+
+  attr_accessor :owner, :mood
+  attr_reader :name
 
   @@all = []
 
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
+    @@all << self
   end
 
   def self.all
-    @all
+    @@all
   end
 end
